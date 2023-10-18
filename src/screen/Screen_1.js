9f06bc2b4f5dc832298c0e566d05e1e36be06a9d
Mihir <mihirghelani.cyber@gmail.com>
2023-10-18 15:07:29 +0530

    added wifi ip

--- src/screen/Screen_1.js
+++ src/screen/Screen_1.js
@@ -1,59 +1,52 @@
-import React, {Component} from 'react';
-import {
-  TextInput,
-  StyleSheet,
-  Text,
-  View,
-  TouchableOpacity,
-  ScrollView,
-} from 'react-native';
+import React, {useEffect} from 'react';
+import {PermissionsAndroid, Text, View} from 'react-native';
+import WifiManager from 'react-native-wifi-reborn';
 
-export default class App extends Component {
-  state = {
-    result: '',
-    command: '',
+export const Screen_1 = () => {
+  useEffect(() => {
+    get_data();
+  }, []);
+
+  const get_data = async () => {
+    const granted = await PermissionsAndroid.request(
+      PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      {
+        title: 'Location permission is required for WiFi connections',
+        message:
+          'This app needs location permission as this is required  ' +
+          'to scan for wifi networks.',
+        buttonNegative: 'DENY',
+        buttonPositive: 'ALLOW',
+      },
+    );
+    if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+      console.log('Permission granted');
+    } else {
+      console.log('Note give permission');
+    }
   };
 
-  render() {
-    const {result} = this.state;
-    return (
-      <ScrollView style={{flex: 1}} contentContainerStyle={styles.container}>
-        <Text style={styles.welcome}>Android Shell example</Text>
-        <Text style={styles.instructions}>RESULT: {result}</Text>
-        <TextInput
-          style={styles.formCommand}
-          onChangeText={x => this.setState({command: x})}
-          keyboardType="default"
-          onSubmitEditing={() => this.onPressEnter()}
-          placeholder="Enter your command"
-        />
-      </ScrollView>
-    );
-  }
-}
+  WifiManager.getIP().then(
+    abc => {
+      console.log('Connected successfully!', abc);
+    },
+    () => {
+      console.log('Connection failed!');
+    },
+  );
 
-const styles = StyleSheet.create({
-  container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-  formCommand: {
-    paddingLeft: 6,
-    marginBottom: 10,
-    borderRadius: 5,
-    borderWidth: 1,
-    borderColor: '#000',
-    width: 300,
-    height: 40,
-  },
-});
+  WifiManager.getCurrentWifiSSID().then(
+    ssid => {
+      console.log('Your current connected wifi SSID is ' + ssid);
+    },
+    () => {
+      console.log('Cannot get current SSID!');
+    },
+  );
+
+  return (
+    <View style={{flex: 1, margin: 10, borderWidth: 1}}>
+      <Text>Hiii Screnn_1</Text>
+    </View>
+  );
+};
